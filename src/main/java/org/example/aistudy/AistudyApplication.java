09fd78e660a55a63784bf7435002df22d6ed3bed
jihoon <git40@gmail.com>
2026-02-20 01:14:32 +0900

    init commit

---
+++ src/main/java/org/example/aistudy/AistudyApplication.java
@@ -0,0 +1,13 @@
+package org.example.aistudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AistudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AistudyApplication.class, args);
+    }
+
+}
